d5840dbf9ea06495583c79907a3667ac07893246
muzhiyun <my616422@126.com>
2023-09-30 16:17:43 +0800

    first commit,FHD@60Hz

---
+++ src/gowin_clkdiv/gowin_clkdiv.mod
@@ -0,0 +1,14 @@
+-series GW5AST
+-device GW5AST-138B
+-device_version B
+-package FCPBGA676A
+-part_number GW5AST-LV138FPG676AC1/I0
+
+
+-mod_name Gowin_CLKDIV
+-file_name gowin_clkdiv
+-path C:/Users/MuZhi/Desktop/zynq/138k/project/HDMI/138k_pattern/src/gowin_clkdiv/
+-type CLKDIV
+-file_type vlg
+-division_factor 5
+-calib false
---
+++ src/gowin_clkdiv/gowin_clkdiv_tmp.v
@@ -0,0 +1,19 @@
+//Copyright (C)2014-2023 Gowin Semiconductor Corporation.
+//All rights reserved.
+//File Title: Template file for instantiation
+//GOWIN Version: V1.9.9 Beta-5
+//Part Number: GW5AST-LV138FPG676AC1/I0
+//Device: GW5AST-138B
+//Device Version: B
+//Created Time: Fri Sep 29 18:52:35 2023
+
+//Change the instance name and port connections to the signal names
+//--------Copy here to design--------
+
+    Gowin_CLKDIV your_instance_name(
+        .clkout(clkout_o), //output clkout
+        .hclkin(hclkin_i), //input hclkin
+        .resetn(resetn_i) //input resetn
+    );
+
+//--------Copy end-------------------
